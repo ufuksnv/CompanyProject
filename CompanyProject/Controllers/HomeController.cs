8cac90f1b26f1df0c838a2abd74f4121035970e6
ufuk murat sınavu <u.murat0606@hotmail.com>
2023-04-03 12:48:30 +0300

    Add SignUp and SignIn

--- CompanyProject/Controllers/HomeController.cs
+++ CompanyProject/Controllers/HomeController.cs
@@ -1,4 +1,7 @@
 ﻿using CompanyProject.Models;
+using CompanyProject.ViewModels;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
@@ -7,16 +10,96 @@ namespace CompanyProject.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly UserManager<AppUser> _userManager;
+        private readonly SignInManager<AppUser> _signInManager;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, UserManager<AppUser> userManager, SignInManager<AppUser> signInManager)
         {
             _logger = logger;
+            _userManager = userManager;
+            _signInManager = signInManager;           
         }
+
 
         public IActionResult Index()
         {
             return View();
         }
+
+        [HttpGet]
+        public IActionResult SignUp()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> SignUp(SignUpViewModel request)
+        {
+            if (ModelState.IsValid)
+            {
+                AppUser user = new AppUser()
+                {
+                    UserName = request.UserName,
+                    PhoneNumber = request.Phone,
+                    Email = request.Email
+                };
+
+                var identityResult = await _userManager.CreateAsync(user, request.Password);
+
+                if (identityResult.Succeeded)
+                {
+                    TempData["SuccessMessage"] = "Üyelik kayıt işlemi başarıyla gerçekleşmiştir";
+                    return RedirectToAction(nameof(HomeController.SignUp));
+                }
+
+                foreach (IdentityError item in identityResult.Errors)
+                {
+                    ModelState.AddModelError(string.Empty, item.Description);
+                }
+
+            }
+            return View(request);
+        }
+
+        [HttpGet]
+        public IActionResult SignIn()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> SignIn(SignInViewModel request)
+        {
+
+            var hasUser = await _userManager.FindByEmailAsync(request.Email);
+
+            if (hasUser == null)
+            {
+                TempData["ErrorMessage"] = "Kullanıcı adınız veya parolanız hatalı lütfen tekrar deneyiniz.";
+                return View();
+            }
+
+            var SignInresult = await _signInManager.PasswordSignInAsync(hasUser, request.Password, request.RememberMe, true);
+
+            if (SignInresult.Succeeded)
+            {
+                return RedirectToAction("Index", "Member");
+            }
+            if (SignInresult.IsLockedOut)
+            {
+                TempData["LockedOutMessage"] = "3 dakika sonra tekrar deneyiniz";
+                return View();
+            }
+            TempData["ErrorMessage"] = "Kullanıcı adınız veya parolanız hatalı lütfen tekrar deneyiniz.";
+            return View(request);
+
+
+
+        }
+
+
+
+
 
         public IActionResult Privacy()
         {
